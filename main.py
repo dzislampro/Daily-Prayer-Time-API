a2f30728742ec258d5479eda006bb77ccbae0058
dzislampro <62948273+dzislampro@users.noreply.github.com>
2026-02-19 11:08:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-from threading import Thread
 import os
-
 from duckduckgo_search import DDGS
 from bs4 import BeautifulSoup
 import dateparser
 
-app = Flask('')
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 
@@ -22,61 +19,42 @@ def get_results(query):
 
 @app.route('/')
 def home():
-  return "I'm alive"
+    return "I'm alive"
 
 
 @app.route('/api/<string:s>', methods=['GET'])
 @cross_origin(origin='*')
 def prayer(s):
-
-
-  query = str(s + " prayer time site:muslimpro.com")
-  data = {}
-
-  # Run the asynchronous function in the event loop
-urls = get_results(query)
-
-  try:
-    url = urls[0]['href']
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    city = soup.find("p", attrs={"class": "location"})
-    dates = soup.find("div", attrs={"class": "prayer-daily-title-location"})
-    data["city"] = city.get_text()
-    data["date"] = dates.get_text()
-    data["today"] = {}
-    data["tomorrow"] = {}
-    waktu = soup.find_all("span", attrs={"class": "waktu-solat"})
-    jam = soup.find_all("span", attrs={"class": "jam-solat"})
-
-    for x, y in zip(waktu, jam):
-      data["today"][x.get_text()] = y.get_text()
-
-    names = ["Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Ishaa"]
+    query = f"{s} prayer time site:muslimpro.com"
+    data = {}
 
     try:
-      tomorrow = soup.find("tr", attrs={
-        "class": "active"
-      }).find_next("tr").find_all("td", attrs={"class": "prayertime"})
-      for x, y in zip(names, tomorrow):
-        data["tomorrow"][x] = y.get_text()
-    except:
-      month = str(dateparser.parse(data["date"]))[5:7]
-      url = url + '?date=2021-' + str(int(month) + 1)
-      response = requests.get(url)
-      soup = BeautifulSoup(response.content, "html.parser")
-      tomorrow = soup.find_all("tr")[1].find_all("td",
-                                                 attrs={"class": "prayertime"})
-      for x, y in zip(names, tomorrow):
-        data["tomorrow"][x] = y.get_text()
-  except Exception as e:
-    print(e)
-    data["Error"] = "Result Not Found"
+        urls = get_results(query)
+        url = urls[0]['href']
 
-  return jsonify(data)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        city = soup.find("p", attrs={"class": "location"})
+        dates = soup.find("div", attrs={"class": "prayer-daily-title-location"})
+
+        data["city"] = city.get_text() if city else "Unknown"
+        data["date"] = dates.get_text() if dates else "Unknown"
+        data["today"] = {}
+        data["tomorrow"] = {}
+
+        waktu = soup.find_all("span", attrs={"class": "waktu-solat"})
+        jam = soup.find_all("span", attrs={"class": "jam-solat"})
+
+        for x, y in zip(waktu, jam):
+            data["today"][x.get_text()] = y.get_text()
+
+    except Exception as e:
+        data["Error"] = "Result Not Found"
+
+    return jsonify(data)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
